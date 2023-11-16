3429b1a6eb7b17f75d5836e78dc5981f1dcaf23c
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2023-11-16 14:50:24 +0100

    id E10T9

--- src/main.cpp
+++ src/main.cpp
@@ -47,27 +47,58 @@ void connectAp() {
               client.println();
               client.println("<!DOCTYPE HTML>");
               client.println("<html>");
+              client.println("<head>");
+              client.println("<style>");
+              client.println("body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f0f0f0; }");
+              client.println("form { max-width: 350px; margin: 50px auto; padding: 20px; background-color: #ffffff; border-radius: 8px; }");
+              client.println("input[type='text'], input[type='password'] { width: 90%; padding: 10px; margin-bottom: 10px; border-radius: 4px; border: 1px solid #ccc; }");
+              client.println("input[type='submit'] { width: 96%; padding: 10px; border-radius: 4px; border: none; color: #ffffff; background-color: #007BFF; cursor: pointer; }");
+              client.println("input[type='submit']:hover { background-color: #0056b3; }");
+              client.println("</style>");
+              client.println("</head>");
+              client.println("<body>");
               client.println("<form method='post' action='/'>");
               client.println("SSID: <input type='text' name='ssid'><br>");
               client.println("Password: <input type='password' name='pass'><br>");
+              client.println("<input type='hidden' name='hiddenField' value='test'>");
               client.println("<input type='submit' value='Connect'>");
               client.println("</form>");
+              client.println("</body>");
               client.println("</html>");
               break;
             } else {
               currentLine = "";
             }
           } else if (c == '\r') {
-            // if the current line is the Content-Length line, get the value
+            // // if the current line is the Content-Length line, get the value
             if (currentLine.startsWith("Content-Length: ")) {
-              int contentLength = currentLine.substring(16).toInt();
-              // read the next (blank) line
               while (client.read() != '\n');
-              // now read the POST data
-              for (int i = 0; i < contentLength; i++) {
-                postData += (char)client.read();
-              }
+              
+             
+                  postData = client.readStringUntil('\n');
+
+                  if (postData.startsWith("ssid="))
+                  {
+                    Serial.println(postData);
+                    int ssidStart = postData.indexOf("ssid=") + 5;
+                    int ssidEnd = postData.indexOf("&", ssidStart);
+                    String newSsid = postData.substring(ssidStart, ssidEnd);
+                    int passStart = postData.indexOf("pass=") + 5;
+                    int passEnd = postData.indexOf("&", passStart);
+                    String newPassword = postData.substring(passStart, passEnd);
+                    ssid[newSsid.length() + 1];
+                    newSsid.toCharArray(ssid, newSsid.length() + 1);
+
+                    pass[newPassword.length() + 1];
+                    newPassword.toCharArray(pass, newPassword.length() + 1);
+                    Serial.println(ssid);
+                    Serial.println(pass);
+                    connectWifi();
+                  }
+
+
             }
+
           }
         }
       }
@@ -75,8 +106,7 @@ void connectAp() {
       client.stop();
       delay(1000);
 
-      // print the POST data to the console
-      Serial.println(postData);
+      
 
     }
   }
@@ -86,9 +116,11 @@ void connectAp() {
 
 
 void connectWifi() {
-  while (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, pass);
+    Serial.print("Connecting to ");
+  while (WiFi.status() != WL_CONNECTED) {
     delay(4000);
+    WiFi.begin(ssid, pass);
     Serial.print(".");
   }
   printNertworkData();
